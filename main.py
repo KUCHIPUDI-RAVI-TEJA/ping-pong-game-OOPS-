b8f01c16b766b481f0a74f066d3df7a17d0c9123
KUCHIPUDI-RAVI-TEJA <62494367+KUCHIPUDI-RAVI-TEJA@users.noreply.github.com>
2021-07-17 09:11:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import ScoreBoard
+
+screen = Screen()
+
+screen.bgcolor("black")
+screen.setup(height=600, width=800)
+screen.title("Pong")
+screen.tracer(0)
+
+scoreboard = ScoreBoard()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_on = True
+while game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect Collision with walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect Collision with paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or \
+            ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # When L paddle misses the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # When R paddle misses the ball
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
